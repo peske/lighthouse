16787e1922d8dcd8fb8001ea195b760382799c42
Aleksandar Pesic <peske@ubuntu-admin.itenlight.com>
2023-01-24 05:54:41 +0100

    implement geth StatefulSet
    Signed-off-by: Aleksandar Pesic <peske@ubuntu-admin.itenlight.com>

--- README.md
+++ README.md
@@ -1,4 +1,61 @@
+> **IMPORTANT: Work in progress!** The repository is not ready for use.
+
 # What?
 
-Kubernetes manifests for [`chaind`](https://github.com/wealdtech/chaind) with 
-[Lighthouse](https://github.com/sigp/lighthouse)
+Kubernetes manifests for deploying
+[Lighthouse](https://github.com/sigp/lighthouse) with
+[`geth`](https://geth.ethereum.org/).
+
+# Resources
+
+- https://github.com/kimpers/lighthouse-docker
+- https://geth.ethereum.org/docs/getting-started/consensus-clients
+
+# Preparation
+
+Create the `lighthouse` namespace:
+
+```bash
+kubectl apply -f ./namespace.yaml
+```
+
+## JWT token
+
+To enable communication between the execution client (`geth`) and the consensus
+client (`lighthouse`) using the new Engine API you need to generate a JWT Token
+to be used for communication. You can create it by executing:
+
+```bash
+./generate-jwt.sh
+```
+
+This will create a `./jwttoken` directory, and `./jwttoken/jwtsecret.hex` file
+in it. The next step is to create a `Secret` named `jwt-token` from the token by
+executing:
+
+```bash
+kubectl --namespace lighthouse create secret generic jwt-token --from-file=./jwttoken/jwtsecret.hex
+```
+
+## Environment variables
+
+The first step is to configure environment variables in [`.env`](./.env) file.
+When it is done, crate a `ConfigMap` from it by executing:
+
+```bash
+kubectl --namespace lighthouse create configmap env-vars --from-env-file=./.env
+```
+
+# Install `geth`
+
+Create a `ConfigMap` with the entry point script by executing:
+
+```bash
+kubectl --namespace lighthouse create configmap start-geth-sh --from-file=./scripts/start-geth.sh
+```
+
+Create a `StatefulSet` by executing:
+
+```bash
+kubectl apply -f ./geth.yaml
+```
---
+++ scripts/start-beacon-node.sh
@@ -0,0 +1,78 @@
+#! /bin/bash
+#
+# Starts a beacon node.
+
+DEFAULT_NETWORK=mainnet
+
+if [ "$START_VALIDATOR" != "" ]; then
+	ETH1_FLAG=--eth1
+fi
+
+if [ "$NETWORK" = "" ]; then
+	NETWORK=$DEFAULT_NETWORK
+fi
+
+if [ "$ENABLE_METRICS" != "" ]; then
+	METRICS_PARAMS="--metrics --metrics-address 0.0.0.0 "
+fi
+
+if [ "$GRAFFITI" != "" ]; then
+	GRAFFITI_PARAM="--graffiti $GRAFFITI"
+fi
+
+if [ "$PRIVATE" != "" ]; then
+	PRIVATE_FLAG="--private"
+fi
+
+if [ "$START_SLASHER" != "" ]; then
+	SLASHER_FLAG="--slasher"
+fi
+
+if [ "$SEARCH_BLOCKS" != "" ]; then
+	SEARCH_BLOCKS_PARAM="--eth1-blocks-per-log-query $SEARCH_BLOCKS"
+fi
+
+if [ "$ENABLE_MONITORING_AUTO" != "" ]; then
+	ENABLE_MONITORING_AUTO_FLAG="--validator-monitor-auto"
+fi
+
+if [ "$ENABLE_MONITORING_MANUAL" != "" ]; then
+	ENABLE_MONITORING_MANUAL_PARAMS="--validator-monitor-pubkeys $ENABLE_MONITORING_MANUAL"
+fi
+
+if [ "$MONITORING_SERVICE_ENDPOINT" != "" ]; then
+	MONITORING_SERVICE_PARAMS="--monitoring-endpoint $MONITORING_SERVICE_ENDPOINT"
+fi
+
+if [ "$ENABLE_FULL_NETWORK_VIEW" != "" ]; then
+	ENABLE_FULL_NETWORK_VIEW_PARAMS="--subscribe-all-subnets --import-all-attestations"
+fi
+
+if [ "$CHECKPOINT_SYNC_URL" != "" ]; then
+	CHECKPOINT_SYNC_URL_PARAM="--checkpoint-sync-url $CHECKPOINT_SYNC_URL"
+fi
+
+if [ "$SUGGESTED_FEE_RECIPIENT" != "" ]; then
+  FEE_RECIPIENT="--suggested-fee-recipient $SUGGESTED_FEE_RECIPIENT"
+fi
+
+exec lighthouse \
+	--debug-level $DEBUG_LEVEL \
+	--network $NETWORK \
+	beacon_node \
+	--http \
+	--http-address 0.0.0.0 \
+	--execution-endpoint $EXECUTION_ENDPOINT \
+	--execution-jwt /root/jwttoken/jwtsecret.hex \
+	$METRICS_PARAMS \
+	$GRAFFITI_PARAM \
+	$ETH1_FLAG \
+	$SLASHER_FLAG \
+	$SEARCH_BLOCKS_PARAM \
+	$PRIVATE_FLAG \
+	$ENABLE_MONITORING_AUTO_FLAG \
+	$ENABLE_MONITORING_MANUAL_PARAMS \
+	$ENABLE_FULL_NETWORK_VIEW_PARAMS \
+	$MONITORING_SERVICE_PARAMS \
+	$CHECKPOINT_SYNC_URL_PARAM \
+	$FEE_RECIPIENT
